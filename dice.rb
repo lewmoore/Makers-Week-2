093cf43a788c1d0ceb888bea861354099721e99f
lewismoore <lewis.moore2707@gmail.com>
2017-11-25 22:21:31 +0000

    Added comments

--- dice.rb
+++ dice.rb
@@ -1,14 +1,14 @@
 def roll_count
-  player_1 = rand(12)
+  player_1 = rand(12) # Each player has 2 sic-sided dice
   player_2 = rand(12)
 
-  if player_1 > player_2
+  if player_1 > player_2 # shows who has one based on values rolled
     puts "Player 1 wins! You rolled: #{player_1}"
   else
     puts "Player 2 wins! You rolled: #{player_2}"
   end
 end
 
-roll_count
+roll_count # best of three rounds
 roll_count
 roll_count
