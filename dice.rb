acfedcd176add32425248d6805a2af08d666c95d
lewismoore <lewis.moore2707@gmail.com>
2017-11-25 22:20:21 +0000

    Dice game

--- dice.rb
+++ dice.rb
@@ -1,3 +1,14 @@
-number = 7
+def roll_count
+  player_1 = rand(12)
+  player_2 = rand(12)
 
-puts rand(number)
+  if player_1 > player_2
+    puts "Player 1 wins! You rolled: #{player_1}"
+  else
+    puts "Player 2 wins! You rolled: #{player_2}"
+  end
+end
+
+roll_count
+roll_count
+roll_count
