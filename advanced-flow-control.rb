2df793c226310bd76f6109d9e649afca43e5403a
lewismoore <lewis.moore2707@gmail.com>
2017-11-27 08:13:02 +0000

    Added 'Advanced Flow Control' program

---
+++ advanced-flow-control.rb
@@ -0,0 +1,13 @@
+# The specification for this program:
+# I hate my mate Steve: so much so that my hatred spills over to anyone with a first name beginning with 'S'.
+# I want a program that, when anyone types their name in, shouts at them if their name begins with an 'S'.
+# Anyone else should just get a friendly greeting.
+
+puts "What is your name?"
+user_input = gets.chomp
+
+if user_input.start_with?("S", "s")
+  puts user_input.upcase
+else
+  puts "Hi #{user_input}."
+end
